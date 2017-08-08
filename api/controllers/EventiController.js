9ac2a560232041db347bfa11406c89a1366d94d8
Andrea <andrea.93@hotmail.it>
2017-08-09 00:15:53 +0200

    non ricordo

---
+++ api/controllers/EventiController.js
@@ -0,0 +1,12 @@
+/**
+ * EventiController
+ *
+ * @description :: Server-side logic for managing eventis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	
+};
+
---
+++ api/models/Eventi.js
@@ -0,0 +1,30 @@
+/**
+ * Eventi.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    artista: {
+      type: 'string',
+      required: true
+    },
+    luogo: {
+      type: 'string',
+      required: true,
+    },
+    data: {
+      type: 'date',
+      required: true,
+    },
+    prezzo: {
+      type: 'float',
+      required: true
+    }
+  }
+
+};
+
